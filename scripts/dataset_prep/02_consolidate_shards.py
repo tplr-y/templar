d76903af056d398c642d9fe43d0519ce47250f3a
Alexander Lavelle <alexander.m.lavelle@vanderbilt.edu>
2025-08-01 10:03:53 -0400

    Debug the awaiting

--- scripts/dataset_prep/02_consolidate_shards.py
+++ scripts/dataset_prep/02_consolidate_shards.py
@@ -63,12 +63,15 @@ async def run_preprocessing(args, seq_len: int = 2048, token_dtype: np.dtype = n
     )
     bucket = comms_.get_own_bucket("dataset", "read")
     tokens_file, ids_file = sharded_dataset.SharedShardedDataset.locate_shards(0)
+    print("Downloading first shard")
     try:
-        await comms_.s3_get_object(
+        downloaded = await comms_.s3_get_object(
             tokens_file,
             bucket,
             load_data=False,
         )
+        print(downloaded, type(downloaded))
+        print("Shard downloaded")
     except Exception as e:
         print(e)
 
